e32a050a8f6dde5df8e880c4cb4c77fa8e1bb1cc
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-29 03:43:32 +0200

    ingredients extracted out of recipes

---
+++ scripts/fonctionnel.js
@@ -0,0 +1,47 @@
+console.log(recipes)
+
+const ingredientsWithDuplicates = recipes.reduce((accu, recipe) => {
+    // reduce extract ingredients arrays
+    // map extract ingredients arrays with only the ingredient in it
+    // spread operator get rid of ingredients container : array containing only ingredient(s) as elements
+    accu.push(...recipe.ingredients.map(ingredient => ingredient.ingredient)) 
+    return accu
+} , [])
+
+console.log(ingredientsWithDuplicates)
+
+console.log( "reduce : ", recipes.reduce((accu, recipe) => {
+    accu.push(recipe.ingredients) 
+    return accu
+}, []))
+
+/* REDUCE
+[[{ingredient: 'Lait de coco', quantity: 400, unit: 'ml'},
+{ingredient: 'Jus de citron', quantity: 2},
+{ingredient: 'Crème de coco', quantity: 2, unit: 'cuillères à soupe'},
+{ingredient: 'Sucre', quantity: 30, unit: 'grammes'},
+{ingredient: 'Glaçons'}], ...]
+*/
+
+console.log( "spread : ", recipes.reduce((accu, recipe) => {
+    accu.push(...recipe.ingredients) 
+    return accu
+}, []))
+
+/* SPREAD
+[{ingredient: 'Lait de coco', quantity: 400, unit: 'ml'},
+{ingredient: 'Jus de citron', quantity: 2},
+{ingredient: 'Crème de coco', quantity: 2, unit: 'cuillères à soupe'},
+{ingredient: 'Sucre', quantity: 30, unit: 'grammes'},
+{ingredient: 'Glaçons'}, ...]
+*/
+
+console.log( "map : ", recipes.reduce((accu, recipe) => {
+    accu.push(...recipe.ingredients.map(ingredient => ingredient.ingredient)) 
+    return accu
+} , []))
+
+/* MAP
+['Lait de coco', 'Jus de citron', 'Crème de coco', ...]
+*/
+
