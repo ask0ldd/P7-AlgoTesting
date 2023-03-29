8aadef0102cb1e2b9f1e631b66cf50b3ec877055
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-30 00:56:24 +0200

    basic functions that will serve as basis for the algos

--- scripts/functional.js
+++ scripts/functional.js
@@ -60,6 +60,9 @@ searchBar.addEventListener('input', (e) => {
     if(searchBar.value.length>2) console.log('RESULTS : ', getRecipesContaining(searchBar.value))
 })
 
+/* KEEP : GET RECIPES CONTAINING WORD */
+// !!trim input value
+
 function getRecipesContaining(text){
     return recipes.filter(recipe => {
         // keep recipe if :
@@ -71,27 +74,30 @@ function getRecipesContaining(text){
 }
 
 function isRecipeNameIncluding(recipe, text){
-    return recipe.name.toLowerCase().includes(text)
+    return recipe.name.toLowerCase().trim().includes(text)
 }
 
 function isRecipeDescriptionIncluding(recipe, text){
-    return recipe.description.toLowerCase().includes(text)
+    return recipe.description.toLowerCase().trim().includes(text)
 }
 
 function areRecipeIngredientsIncluding(recipe, text){
-    return recipe.ingredients.filter(ingredient => ingredient.ingredient.toLowerCase().includes(text)).length > 0
+    return recipe.ingredients.filter(ingredient => ingredient.ingredient.toLowerCase().trim().includes(text)).length > 0
 }
 
-function getFullIngredientsArrays (recipes) { // ingredient + quantity + unit for each entry
+/*function getFullIngredientsArrays (recipes) { // ingredient + quantity + unit for each entry
     return recipes.reduce((accu, recipe) => {
         accu.push(recipe.ingredients)
         return accu
     }, [])
-}
+}*/
+
+/* KEEP : GET INGREDIENTS WITH NO DUPLICATES */
 
 const getIngredientsOnly = (onerecipe) => { // return an array of ingredients extracted out of a recipe
     return onerecipe.ingredients.map(array => array.ingredient)
 }
+
 
 function getAllIngredientsWDuplicates(recipes){
     return recipes.reduce((accu, recipe) => {
@@ -100,19 +106,21 @@ function getAllIngredientsWDuplicates(recipes){
     }, [])
 }
 
-// get ingredients arrays only : reduce
-/* AFTER REDUCE
-[1:[1:{ingredient: 'Lait de coco', quantity: 400, unit: 'ml'},
+/* 
+***get ingredients arrays only : reduce
+AFTER REDUCE
+[1 : [1:{ingredient: 'Lait de coco', quantity: 400, unit: 'ml'},
 2:{ingredient: 'Jus de citron', quantity: 2},
 3:{ingredient: 'Crème de coco', quantity: 2, unit: 'cuillères à soupe'},
 4:{ingredient: 'Sucre', quantity: 30, unit: 'grammes'},
-5:{ingredient: 'Glaçons'}], 2:[...] ...]
-*/
-// extract ingredients (duplicates included) of ingredients arrays : getIngredientsOnly()
-/* AFTER MAP
+5:{ingredient: 'Glaçons'}], 2 : [...], ...]
+
+***extract ingredients (duplicates included) of ingredients arrays : getIngredientsOnly()
+AFTER MAP
 [1:'Lait de coco', 2:'Jus de citron', 3:'Crème de coco', ...]
+
+***remove duplicates from ingredients array : removeDuplicates()
 */
-// remove duplicates from ingredients array : removeDuplicates()
 function getAllIngredientsNoDuplicates(recipes){
     return removeDuplicates(recipes.reduce((accu, recipe) => {
         accu.push(...getIngredientsOnly(recipe))
@@ -122,7 +130,7 @@ function getAllIngredientsNoDuplicates(recipes){
 
 function getIngredientsArray(recipes, ){
     return recipes.reduce((accu, recipe) => {
-        accu.push(recipe)
+        accu.push(recipe.ingredients)
         return accu
     }, [])
 }
@@ -131,4 +139,45 @@ function removeDuplicates(array){
     return array.filter((element, index, array) => array.indexOf(element) === index)
 }
 
-console.log(getArrayWithIngredientsNoDuplicates(recipes))
+// Array.prototype.findIndex()
+
+/*
+ function to get {recipeID, [ingredient1,...]}
+*/
+
+function getRecipesIdAndFullIngredientsArray(recipes){
+    return recipes.reduce((accu, recipe) => {
+        accu.push({'recipeid' : recipe.id, 'ingredients' : recipe.ingredients})
+        return accu
+    }, [])
+}
+/* returns :
+[{ingredients: [
+0 : {ingredient: 'Lait de coco', quantity: 400, unit: 'ml'},
+1 : {ingredient: 'Jus de citron', quantity: 2},
+2 : {ingredient: 'Crème de coco', quantity: 2, unit: 'cuillères à soupe'},
+3 : {ingredient: 'Sucre', quantity: 30, unit: 'grammes'},
+4 : {ingredient: 'Glaçons'}],
+recipeid : 1 }, ...]
+*/
+
+function toIngredientsList(ingredientsArray){
+    return ingredientsArray.map(ArrElt => ArrElt.ingredient)
+}
+
+function getRecipesIdAndListOfIngredients(recipes){
+    return recipes.reduce((accu, recipe) => {
+        accu.push({
+            'recipeid' : recipe.id, 
+            // from complex array with ingredient/quantity/unit, to a simple list of ingredients
+            'ingredients' : toIngredientsList(recipe.ingredients) 
+        })
+        return accu
+    }, [])
+}
+
+console.log(getRecipesIdAndListOfIngredients(recipes))
+
+/*const ingredients = getAllIngredientsNoDuplicates(recipes)
+const ingredientsSelect = document.querySelector('#select-ingredients')
+ingredientsSelect.innerHTML = ingredients.reduce((accu, ingredient) => accu+`<option value="${}">${ingredient}</option>`, '')*/
