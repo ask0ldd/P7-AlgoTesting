9c48773de8f0a8e48b0e7aa61e655214aafb07f3
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-03-30 04:12:18 +0200

    array intersection

--- scripts/functional.js
+++ scripts/functional.js
@@ -1,27 +1,38 @@
 let activeRecipes = recipes
 
-function isRecipeNameIncluding(recipe, text){
+function doesRecipeNameContains(recipe, text){
     return recipe.name.toLowerCase().trim().includes(text)
 }
 
-function isRecipeDescriptionIncluding(recipe, text){
+function doesRecipeDescriptionContains(recipe, text){
     return recipe.description.toLowerCase().trim().includes(text)
 }
 
-function areRecipeIngredientsIncluding(recipe, text){
+function doesRecipeIngredientsContain(recipe, text){
     return recipe.ingredients.filter(ingredient => ingredient.ingredient.toLowerCase().trim().includes(text)).length > 0
 }
 
 function searchbarFiltering(recipes, filter){
     return recipes.filter(recipe => {
-        return isRecipeNameIncluding(recipe, filter) || isRecipeDescriptionIncluding(recipe, filter) || areRecipeIngredientsIncluding(recipe, filter)
+        return doesRecipeNameContains(recipe, filter) || doesRecipeDescriptionContains(recipe, filter) || doesRecipeIngredientsContain(recipe, filter)
     })
 }
 
+function intersectTwoRecipesArrays(recipesArray1, recipesArray2){
+    // iterate on the smaller array for optimisation purposes
+    return recipesArray1.length < recipesArray2.length ? recipesArray1.filter(recipe => recipesArray2.includes(recipe)) : recipesArray2.filter(recipe => recipesArray1.includes(recipe))
+}
+
 const searchBar = document.querySelector('.mainSearchBar')
+const resultsContainer = document.querySelector('.searchResults')
+
 searchBar.addEventListener('input', (e) => {
     if(searchBar.value.length>2) {
         activeRecipes = searchbarFiltering(recipes, searchBar.value)
-        console.log('RESULTS : ', activeRecipes)
+    }else{
+        activeRecipes = recipes
     }
+    resultsContainer.innerHTML = activeRecipes.reduce((accu, recipe) => accu + `<article>${recipe?.id}<br>${recipe?.name}<br>${recipe?.description}</article>`, '')
 })
+
+console.log(intersectTwoRecipesArrays(recipes, activeRecipes))
