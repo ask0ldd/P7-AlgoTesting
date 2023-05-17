4ea115248b457a077af7c86fdeb92a126410866c
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-17 22:14:09 +0200

    removing unused files

--- scripts/factory/tagsFactory.js
+++ scripts/factory/tagsFactory.js
@@ -2,7 +2,7 @@ import tagsShelf from "../components/tagsShelf.js"
 import { FirstLetterMaj } from "../utils/stringUtils.js"
 import recipesGallery from "../components/recipesGallery.js"
 import filteringChain from "../utils/filteringChain.js"
-import { closeAllOptionsContainers } from "../functional.js"
+import { closeAllOptionsContainers } from "../index.js"
 
 const tagsFactory = function ({tagName, tagType}) 
 {
