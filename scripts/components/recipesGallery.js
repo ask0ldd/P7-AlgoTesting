58ac218f4c1ebeb3caa9fd5849abd735fde54fe6
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-05-19 19:55:10 +0200

    better recipesfactory implementation

--- scripts/components/recipesGallery.js
+++ scripts/components/recipesGallery.js
@@ -4,7 +4,11 @@ const recipesGallery = {
     node : document.querySelector('.searchResults'),
 
     refresh : function(RecipesAdapter) {
-        this.node.innerHTML = `<div style="font-family:lato; position : absolute; top:2rem; left:93%; width:100%; height:3rem;">${RecipesAdapter.nRecipes} resultats</div>` + RecipesAdapter.recipes.reduce((accu, recipe) => accu + recipesFactory.buildCardView(recipe), '')
+        this.node.innerHTML = `<div style="font-family:lato; position : absolute; top:2rem; left:93%; width:100%; height:3rem;">${RecipesAdapter.nRecipes} resultats</div>` 
+        + RecipesAdapter.recipes.reduce((accu, recipe) => {
+            if(!recipe?.ingredients || !recipe?.name || !recipe?.description) return accu
+            return accu + recipesFactory(recipe)?.buildCardView()
+        }, '')
     }
 }
 
--- scripts/factory/recipesFactory.js
+++ scripts/factory/recipesFactory.js
@@ -1,36 +1,44 @@
-const recipesFactory = 
+const recipesFactory = (recipe) =>
 {
-    buildCardView(recipe) {
-        // ingredients list which will be injected into the recipe view
-        const ingredientsList = (recipe.ingredients).reduce((accu, ingredient, currentIndex) => {
-            if(currentIndex > 4 ) return accu // max 6 ingredients displayed on the card
-            let ingredientString = ""
-            if(ingredient?.quantity) ingredientString += `: ${ingredient.quantity}`
-            if(ingredient?.unit) ingredientString += ` ${ingredient.unit.split(' ')[0].replace('grammes', 'g')}`
-            return (accu + `<li>${ingredient?.ingredient}<span style="font-weight:400;">${ingredientString}</span></li>`)
-            // return (accu + `<li>${ingredient?.ingredient}${ingredient?.quantity || ingredient?.unit ? ': ' : ''} ${ingredient?.quantity ? ingredient?.quantity : ''} ${ingredient?.unit ? ingredient?.unit.split(' ')[0].replace('grammes', 'g') : ''}</li>`)
-        }, '')
 
-        // recipe view which will be injected into the results gallery
-        const cardTemplate = `
-            <article class="recipeCardContainer">
-                <ul class="recipePop">${ingredientsList}</ul>
-                <div class="recipeImg"></div>
-                <div class="recipeDatasContainer">
-                    <div class="recipeHeadingDurationContainer">
-                        <h2 class="recipeHeading">${recipe?.name}</h2><span class="recipeDuration"><img class="clock" src="./assets/icons/clock.png"/>${recipe?.time} min</span>
+    const recipeInstance = {
+
+        recipeDatas : recipe,
+
+        buildCardView : function () {
+            // ingredients list which will be injected into the recipe view
+            const ingredientsList = (this.recipeDatas?.ingredients).reduce((accu, ingredient, currentIndex) => {
+                if(currentIndex > 4 ) return accu // max 6 ingredients displayed on the card
+                let ingredientString = ""
+                if(ingredient?.quantity) ingredientString += `: ${ingredient.quantity}`
+                if(ingredient?.unit) ingredientString += ` ${ingredient.unit.split(' ')[0].replace('grammes', 'g')}`
+                return (accu + `<li>${ingredient?.ingredient}<span style="font-weight:400;">${ingredientString}</span></li>`)
+                // return (accu + `<li>${ingredient?.ingredient}${ingredient?.quantity || ingredient?.unit ? ': ' : ''} ${ingredient?.quantity ? ingredient?.quantity : ''} ${ingredient?.unit ? ingredient?.unit.split(' ')[0].replace('grammes', 'g') : ''}</li>`)
+            }, '')
+
+            // recipe view which will be injected into the results gallery
+            const cardTemplate = `
+                <article class="recipeCardContainer">
+                    <ul class="recipePop">${ingredientsList}</ul>
+                    <div class="recipeImg"></div>
+                    <div class="recipeDatasContainer">
+                        <div class="recipeHeadingDurationContainer">
+                            <h2 class="recipeHeading">${this.recipeDatas?.name}</h2><span class="recipeDuration"><img class="clock" src="./assets/icons/clock.png"/>${this.recipeDatas?.time || '0'} min</span>
+                        </div>
+                        <div class="recipeIngredientsDesc">
+                            <ul>
+                                ${ingredientsList}
+                            </ul>
+                            <div class="recipeDesc">${this.recipeDatas?.description?.length > 170 ? this.recipeDatas?.description?.substring(0,170) + '...' : this.recipeDatas?.description}</div>
+                        </div>
                     </div>
-                    <div class="recipeIngredientsDesc">
-                        <ul>
-                            ${ingredientsList}
-                        </ul>
-                        <div class="recipeDesc">${recipe?.description?.length > 170 ? recipe?.description?.substring(0,170) + '...' : recipe?.description}</div>
-                    </div>
-                </div>
-            </article>
-        `
-        return cardTemplate
+                </article>
+            `
+            return cardTemplate
+        }
     }
+
+    return recipeInstance
 }
 
 export default recipesFactory
